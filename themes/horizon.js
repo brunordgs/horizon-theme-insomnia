2a110009f6dc1107693a57b75aaef87226a25492
Bruno Rodrigues <brunohrsilva1@gmail.com>
2021-04-09 16:57:07 -0300

    Initial commit

---
+++ themes/horizon.js
@@ -0,0 +1,61 @@
+module.exports = {
+	name: 'horizon',
+	displayName: 'Horizon',
+	theme: {
+		background: {
+			default: '#1C1E26',
+			success: '#29D398',
+			notice: '#FAB795',
+			warning: '#F09483E6',
+			danger: '#E95678',
+			surprise: '#EE64AC',
+			info: '#26BBD9',
+		},
+		foreground: {
+			default: '#D5D8DA',
+			success: '#D5D8DA',
+			notice: '#D5D8DA',
+			warning: '#D5D8DA',
+			danger: '#D5D8DA',
+			surprise: '#D5D8DA',
+			info: '#D5D8DA',
+		},
+		highlight: {
+			default: 'rgba(108, 111, 147, .5)',
+			xxs: 'rgba(108, 111, 147, 0.05)',
+			xs: 'rgba(108, 111, 147, 0.1)',
+			sm: 'rgba(108, 111, 147, 0.2)',
+			md: 'rgba(108, 111, 147, 0.4)',
+			lg: 'rgba(108, 111, 147, 0.6)',
+			xl: 'rgba(108, 111, 147, 0.8)',
+		},
+		styles: {
+			sidebar: {
+				background: {
+					default: '#16161C',
+				},
+			},
+			dialog: {
+				background: {
+					default: '#16161C',
+				},
+			},
+			paneHeader: {
+				background: {
+					default: '#1C1E26',
+					success: '#29D398',
+					notice: '#FAB795',
+					warning: '#F09483E6',
+					danger: '#E95678',
+					surprise: '#EE64AC',
+					info: '#26BBD9',
+				},
+			},
+			transparentOverlay: {
+				background: {
+					default: 'rgba(108, 111, 147, 0.5)',
+				},
+			},
+		},
+	},
+};
